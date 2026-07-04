0d239bf3c5e28e8ed0ac605edc7d4cc3b7fc44da
Neha Suryawanshi <nehasuryawanshi997799email@gmail.com>
2026-07-05 01:26:27 +0530

    Try Node.js runtime for middleware

--- cpstream-frontend/middleware.ts
+++ cpstream-frontend/middleware.ts
@@ -1,23 +1,15 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  // Public routes that don't force a sign-in authentication wall
   publicRoutes: [
     "/",
     "/search",
     "/api/webhooks/clerk",
     "/api/webhooks/livekit",
     "/api/uploadthing",
-    "/:username" // Allows public stream profiles to be viewed without a crash
   ],
 });
 
 export const config = {
-  matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    // Always run for API routes
-    "/(api|trpc)(.*)",
-  ],
-  runtime: "nodejs",
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
